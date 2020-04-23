2e9b1ad038aa4b792b14fc83e6a9b267873fc5ad
Alexander Llanes <alexander.llanes@nareia.com.uy>
2020-04-23 14:03:43 -0300

    Add project files.

---
+++ PancakeViewSamples/PancakeViewSamples/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using PancakeViewSamples.Views;
+using Xamarin.Forms;
+
+namespace PancakeViewSamples
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PancakeViewSamples/PancakeViewSamples/ViewModels/ShadowsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PancakeViewSamples.ViewModels
+{
+    public class ShadowsViewModel : BaseViewModel
+    {
+        public ShadowsViewModel()
+        {
+            Title = "Shadows";
+        }
+    }
+}
